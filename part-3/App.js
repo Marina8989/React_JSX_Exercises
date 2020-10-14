5ab8a376baca813478830496093f41e727f6d70b
Marina8989 <marina.mill209@gmail.com>
2020-10-14 10:00:09 -0700

    react practice

---
+++ part-3/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div>
+            <Person  name="Bill" age={30} hobbies={["singing", "running"]}/>
+            <Person name="Tom" age={38} hobbies={["cooking", "swimming"]} />
+            <Person name="Sarah" age={17} hobbies={["dancing", "reading"]} />
+        </div>
+    )
+}
---
+++ part-3/Person.js
@@ -0,0 +1,17 @@
+const Person = (props) => {
+     let vote = props.age >= 18 ? 'please go vote!' : 'please continue studying'
+     let hobbies = props.hobbies.map(hobby => <li>{hobby}</li>)
+    return (
+        <div>
+            <p>Learn some information about this person</p>
+            <ul>
+                <li>Name: {props.name.slice(0,6)}</li>
+                <li>Age: {props.age}</li>
+                <ul>
+                    Hobbies {hobbies}
+                </ul>
+            </ul>
+            <h3>{vote}</h3>
+        </div>
+    )
+}
